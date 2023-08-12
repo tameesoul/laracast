11b06fb797326397b34c85358d46c20c67101722
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2023-08-12 18:09:39 +0300

    Database Seeding Saves Time

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Category;
+use App\Models\User;
+use App\Models\Post;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,11 +14,41 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
+     $user = User::factory()->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $family = Category::create([
+            "name"=>"Family" , 
+            "slug"=>"family"
+        ]);
+         $personal = Category::create([
+            "name"=>"Personal" , 
+            "slug"=>"personal"
+        ]);
+         $work = Category::create([
+            "name"=>"Work" , 
+            "slug"=>"work"
+        ]);
+        $post = Post::create([
+            "user_id" => $user->id,
+            "category_id" => $personal->id,
+            "title" => "mypersonalpost",
+            "slug" => "personalpost",
+            "excerpt" => "this is just a demo",
+            "body" => "demo demo demo demo"
+        ]);
+        $post = Post::create([
+            "user_id" => $user->id,
+            "category_id" => $family->id,
+            "title" => "myfamilypost",
+            "slug" => "familypost",
+            "excerpt" => "this is just a demo",
+            "body" => "demo demo demo demo"
+        ]);
+        
+        // Now you can access $post->id
+        $this->command->info("Post created with ID: " . $post->id);
     }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,11 +2,10 @@
 <article>
          @foreach($posts as $post)  
 
+         by<a href="{{$post->user->name}}">{{$post->user->name}}</a>
         <h1> 
             <a href="/post/{{$post->slug}}">
-
           {{$post->title}} 
-        
           </a>
         </h1>
         <a href="categories/{{$post->category->slug}}">{{$post->Category->name}}</a>
