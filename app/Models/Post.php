02b049c660f0868cb7fecb7a65a3d02f26d30fc0
ahmedtamee <123337830+tameesoul@users.noreply.github.com>
2023-08-13 14:40:41 +0300

    N+1 problem

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,6 +9,7 @@ class Post extends Model
 {
     use HasFactory;
 
+    protected $with = ["category" , "author"];
     protected $guarded = ["id"];
    // protected $fillable = ['title', 'excerpt', 'body'];
 
